5a6c0abe3dc179a36b747d8c29c1d097ca97ad78
Christian Sørensen <chri27r9@mail.hansenberg.dk>
2023-12-05 14:48:39 +0100

    Lært datatyper med bool

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+/*Datatypes
+int, float,string, bool.
+
+exemple int x = 4;
+        datatype variable_navn = værdi;
+*/
+
+float x;
+x = 4.5f;
+
+int y = 5, z = 6;
+
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+Console.Write("math:");
+Console.WriteLine(x + y + z);
+
+int e = Convert.ToInt16(x);
+Console.WriteLine(e);
